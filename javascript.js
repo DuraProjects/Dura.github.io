e8e26a9c6f63d00369a9c9d29c35827830498b98
Lidura <110614320+DuraProjects@users.noreply.github.com>
2025-04-17 14:22:28 -0500

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const studentElement = document.getElementById('student');
-    const rect = studentElement.getBoundingClientRect();
-    
-    if (rect.top < window.innerHeight && rect.bottom >= 0) {
-        studentElement.classList.add('visible');
-    }
-});
-
